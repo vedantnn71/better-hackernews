c67ba18e1acb128e33efc661843feb8c0f18700b
Vedant Nandwana <vedantnn7.git@gmail.com>
2022-10-20 20:17:11 +0530

    feat: implement mobileMenu

---
+++ src/lib/components/mobileMenu.svelte
@@ -0,0 +1,42 @@
+<script>
+	import cx from 'classnames';
+	import { isOpen } from '$lib/store';
+
+	let open = false;
+
+  const links = {
+    Top: '/top',
+    New: '/new',
+    Show: '/show',
+    Ask: '/ask',
+    Jobs: '/jobs'
+  };
+
+	isOpen.subscribe((val) => {
+		open = val;
+	});
+</script>
+
+{#if open}
+	<div>
+		<div class="space-y-1 px-2 pt-2 pb-3">
+			{#each Object.entries(links) as [name, href]}
+				<a
+					{href}
+					class={cx(
+						'text-gray-800 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-xl text-base font-medium',
+						href === (typeof window !== 'undefined' ? window.location.pathname : false) &&
+							'bg-gray-800 text-white'
+					)}
+				>
+					{name}
+				</a>
+			{/each}
+
+			<button class="btn btn-dark w-full">
+				<div class="gg-moon" />
+				Light Mode
+			</button>
+		</div>
+	</div>
+{/if}
--- src/lib/components/nav.svelte
+++ src/lib/components/nav.svelte
@@ -1,16 +1,36 @@
-<script lang="ts" context="module">
-  import Logo from "./logo.svelte";
+<script lang="ts">
+	import Logo from './logo.svelte';
+	import MobileMenu from './mobileMenu.svelte';
+	import { isOpen } from '$lib/store';
+
+	const links = {
+		Top: '/top',
+		New: '/new',
+		Show: '/show',
+		Ask: '/ask',
+		Jobs: '/jobs'
+	};
 </script>
 
-<div class="flex justify-between items-center w-full py-4 px-8 border-b border-gray-300">
-  <Logo />
-  
-  <div class="flex gap-8">
-    <a href="/top" class="hover:text-gray-700 text-gray-800">Top</a>
-    <a href="/new" class="hover:text-gray-700 text-gray-800">New</a>
-    <a href="/show" class="hover:text-gray-700 text-gray-800">Show</a>
-    <a href="/ask" class="hover:text-gray-700 text-gray-800">Ask</a>
-    <a href="/jobs" class="hover:text-gray-700 text-gray-800">Jobs</a>
-  </div>
+<div class="flex items-center w-full py-4 px-8 border-b border-gray-300">
+	<Logo />
+
+	<div class="hidden md:flex justify-center items-center ml-auto gap-8">
+		{#each Object.entries(links) as [name, href]}
+			<a {href} class="hover:text-gray-700 text-gray-800">{name}</a>
+		{/each}
+
+		<div class="h-4 w-px bg-gray-300" />
+
+		<button class="btn btn-dark py-3 px-5">
+			<div class="gg-moon" />
+			Light Mode
+		</button>
+	</div>
+
+	<button class="ml-auto md:hidden outline-none h-8" on:click={() => isOpen.update((val) => !val)}>
+		<div class="gg-menu" />
+	</button>
 </div>
 
+<MobileMenu />
