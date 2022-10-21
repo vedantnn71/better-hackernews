b8bc3470928a07eb520311726f9622b3b27236e6
Vedant Nandwana <vedantnn7.git@gmail.com>
2022-10-21 09:17:34 +0530

    feat: implement theme store

---
+++ src/lib/store/theme.ts
@@ -0,0 +1,26 @@
+import { writable } from "svelte/store";
+
+export const theme = writable(getLocalStorage("theme") || "light");
+
+export function toggleTheme() {
+  theme.update((value) => {
+    if (value === "light") {
+      setLocalStorage("theme", "dark");
+      return "dark";
+    }
+
+      setLocalStorage("theme", "light");
+    return "light";
+  });
+}
+
+function setLocalStorage (key: string, value: string) {
+  if (typeof window === "undefined") return;
+  localStorage.setItem(key, value);
+}
+
+function getLocalStorage (key: string) {
+  if (typeof window === "undefined") return;
+  return localStorage.getItem(key);
+}
+
