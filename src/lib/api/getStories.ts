e06ce3019d4bd992a6a05f47995848d0b6b43325
Vedant Nandwana <vedantnn7.git@gmail.com>
2022-10-22 13:29:33 +0530

    style(getStories): format code

--- src/lib/api/getStories.ts
+++ src/lib/api/getStories.ts
@@ -8,10 +8,7 @@ import {
 import type { StoryKey } from "$lib/types";
 import axios from "axios";
 
-export async function getStories(
-  storyKey: StoryKey = "top",
-  pageParam = 1
-): Promise<number[]> {
+export async function getStories(storyKey: StoryKey = "top", pageParam = 1): Promise<number[]> {
   const url = getUrl(storyKey);
   const resp = await axios.get<number[]>(url);
   const stories = resp.data.slice(0, pageParam);
