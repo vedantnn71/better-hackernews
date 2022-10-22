d79fe58458b479039c58c06b1e1b2ca57845a558
Vedant Nandwana <vedantnn7.git@gmail.com>
2022-10-22 19:11:33 +0530

    feat(loading): add fade transition

--- src/lib/components/loading.svelte
+++ src/lib/components/loading.svelte
@@ -1,9 +1,11 @@
 <script lang="ts">
   import Logo from "$lib/components/logo.svelte";
+  import { fade } from "svelte/transition";
 </script>
 
 <div
   class="fixed top-0 left-0 grid h-screen min-h-screen w-screen place-items-center bg-gray-100 dark:bg-gray-900"
+  transition:fade
 >
   <div>
     <Logo />
