2b97a446cd373d83fa0467f49d9c70537ba88308
Vedant Nandwana <vedantnn7.git@gmail.com>
2022-10-22 15:50:35 +0530

    fix: inconsistent margins and paddings

--- src/lib/components/nav.svelte
+++ src/lib/components/nav.svelte
@@ -15,7 +15,9 @@
   onDestroy(unsubscribeTheme);
 </script>
 
-<div class="flex w-full items-center border-b border-gray-300 py-4 px-6 md:px-8 dark:border-gray-800">
+<div
+  class="flex w-full items-center border-b border-gray-300 py-4 px-6 dark:border-gray-800 md:px-8"
+>
   <a href="/">
     <Logo />
   </a>
