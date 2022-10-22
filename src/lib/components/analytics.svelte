3e0bd2e1615c7b9698d73c8b1dbcb50b3411e38a
Vedant Nandwana <vedantnn7.git@gmail.com>
2022-10-22 18:11:55 +0530

    perf: implement partytown w/ google analytics

---
+++ src/lib/components/analytics.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+  import { page } from "$app/stores";
+
+  let measurementId = "G-BRDV30JPXQ";
+
+  $: {
+    if (typeof gtag !== "undefined") {
+      gtag("config", "MEASUREMENT_ID", {
+        page_title: document.title,
+        page_path: $page.url.pathname
+      });
+    }
+  }
+</script>
+
+<svelte:head>
+  <script async src="https://www.googletagmanager.com/gtag/js?id={measurementId}">
+  </script>
+  <script>
+    window.dataLayer = window.dataLayer || [];
+
+    function gtag() {
+      dataLayer.push(arguments);
+    }
+
+    gtag("js", new Date());
+    gtag("config", "{measurementId}");
+  </script>
+</svelte:head>
