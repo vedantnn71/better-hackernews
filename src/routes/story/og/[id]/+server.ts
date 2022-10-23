45146e03590b89e1379b5868a912f83fe64b688b
Vedant Nandwana <vedantnn7.git@gmail.com>
2022-10-23 15:47:29 +0530

    fix(server): return fallback image on invalid story id

--- src/routes/story/og/[id]/+server.ts
+++ src/routes/story/og/[id]/+server.ts
@@ -6,14 +6,14 @@ import { Resvg } from "@resvg/resvg-js";
 import { html as toReactElement } from "satori-html";
 import type { Story } from "$lib/types";
 
-const fontFile = await fetch("http://localhost:5173/inter-medium.ttf");
+const fontFile = await fetch("http://hn.vedantnandwana.me/inter-medium.ttf");
 const fontData: ArrayBuffer = await fontFile.arrayBuffer();
 
 const height = 630;
 const width = 1200;
 
 export const GET: RequestHandler = async ({ params }) => {
-  const story = await getStory(+params.id);
+  const story = (await getStory(+params.id)) || null;
   const html = getHtml(story);
 
   const svg = await satori(html, {
@@ -44,7 +44,21 @@ export const GET: RequestHandler = async ({ params }) => {
   });
 };
 
-function getHtml(story: Story) {
+function getHtml(story: Story | null) {
+  if (story === null) {
+    return toReactElement(`
+      <div tw="flex w-full h-full bg-gray-50 p-20 items-center justify-center">
+        <svg xmlns="http://www.w3.org/2000/svg" width="101" height="100" viewBox="0 0 101 100" fill="none">
+          <rect x="0.5" width="100" height="100" rx="16.6667" fill="#F97316"/>
+          <path d="M27.3731 26.0152H38.8551L49.911 46.8958H50.3845L61.4403 26.0152H72.9223L55.2377 57.3598V74.5H45.0578V57.3598L27.3731 26.0152Z" fill="#F9FAFB"/>
+        </svg>
+        <h1 tw="ml-6 text-7xl text-gray-900 font-bold mt-8">
+          Hacker News
+        </h1>
+      </div>
+    `);
+  }
+
   const { title, by, time, score, descendants } = story;
 
   return toReactElement(`
