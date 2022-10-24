e893db51c37e0c4f9ee1551e8fdb51bdf52286fc
Vedant Nandwana <vedantnn7.git@gmail.com>
2022-10-25 00:16:10 +0530

    chore(extension/main): rm obsolete tailwind colors

--- extension/extension/content/main.js
+++ extension/extension/content/main.js
@@ -1,16 +1,3 @@
-// gray colors from tailwindcss
-const colors = {
-  gray50: "#F9FAFB",
-  gray100: "#F3F4F6",
-  gray200: "#E5E7EB",
-  gray300: "#D1D5DB",
-  gray400: "#9CA3AF",
-  gray500: "#6B7280",
-  gray600: "#4B5563",
-  gray700: "#374151",
-  gray800: "#1F2937",
-  gray900: "#111827"
-};
 
 // Utility function to remove DOM element
 const removeElement = (id) => {
