228515051f9fb4e8f2b5c89168334c16d55023a3
Vedant Nandwana <vedantnn7.git@gmail.com>
2022-10-26 04:52:01 +0530

    chore(extension): change upvote icon

--- extension/extension/content/main.js
+++ extension/extension/content/main.js
@@ -307,7 +307,7 @@ function itemElement(item) {
   let upvoteIcon = upvoteIconElement();
 
   if (item.isUpvoted) {
-    upvoteIcon = upvotedIconElement();
+    upvoteIcon.classList += " upvoted";
   }
 
   upvoteIcon.classList += " upvote-icon";
@@ -373,13 +373,23 @@ function createPath() {
 
 function upvoteIconElement() {
   const svg = createSvg();
-  const path = createPath();
-  path.setAttributeNS(
+  const firstPath = createPath();
+  const secondPath = createPath();
+
+  firstPath.setAttributeNS(
     null,
     "d",
-    "M12.781 2.375c-.381-.475-1.181-.475-1.562 0l-8 10A1.001 1.001 0 0 0 4 14h4v7a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1v-7h4a1.001 1.001 0 0 0 .781-1.625l-8-10zM15 12h-1v8h-4v-8H6.081L12 4.601 17.919 12H15z"
+    "M3.37,12.36l7.86-9.44a1,1,0,0,1,1.54,0l7.86,9.44A1,1,0,0,1,19.86,14H16v6a2,2,0,0,1-2,2H10a2,2,0,0,1-2-2V14H4.14A1,1,0,0,1,3.37,12.36Z"
   );
-  svg.appendChild(path);
+
+  secondPath.setAttributeNS(
+    null,
+    "d",
+    "M3.37,12.36l7.86-9.44a1,1,0,0,1,1.54,0l7.86,9.44A1,1,0,0,1,19.86,14H16v6a2,2,0,0,1-2,2H10a2,2,0,0,1-2-2V14H4.14A1,1,0,0,1,3.37,12.36Z"
+  );
+
+  svg.appendChild(firstPath);
+  svg.appendChild(secondPath);
 
   return svg;
 }
